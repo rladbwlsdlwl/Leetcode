433aa33044720accc0e6d2219f934b029c51a7f1
rladbwlsdlwl <rladbwlsdlwl@naver.com>
2020-10-17 21:25:43 +0900

    October17th

--- RepeatedDNASequences.py
+++ RepeatedDNASequences.py
@@ -1,7 +1,7 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
         table={}
-        for i in range(len(s)-10+1):
+        for i in range(len(s)-10+1): # push all substring
             value=s[i:i+10]
             if(value in table):
                 table[value]+=1
