f4aacc7cf36a86d7cda92572df33a040246cae23
rladbwlsdlwl <rladbwlsdlwl@naver.com>
2020-10-17 21:21:37 +0900

    October17th

---
+++ RepeatedDNASequences.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        table={}
+        for i in range(len(s)-10+1):
+            value=s[i:i+10]
+            if(value in table):
+                table[value]+=1
+            else:
+                table[value]=1
+                
+        return [p for p in table.keys() if(table[p]>1)]
