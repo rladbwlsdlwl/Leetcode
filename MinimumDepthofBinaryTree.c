4e31a4de815cd1dd61a0e72d0e767795a690bfbd
rladbwlsdlwl <rladbwlsdlwl@naver.com>
2020-10-23 18:11:39 +0900

    October22nd

--- MinimumDepthofBinaryTree.c
+++ MinimumDepthofBinaryTree.c
@@ -7,21 +7,23 @@
  * };
  */
 
+int min(int p,int q){
+    return (p>q ? q : p);
+}
+
 int minDepth(struct TreeNode* root){
     if(!root)
         return 0;
-    else if(!root->left && !root->right)
+    else if(!root->left && !root->right) // if leaf node, return mine
         return 1;
-    else if(!root->left)
+    else if(!root->left) // if child node 1, find height of node ~ leafnode 
         return 1+minDepth(root->right);
     else if(!root->right)
         return 1+minDepth(root->left);
     else{
-        int left=minDepth(root->left);
+        int left=minDepth(root->left); // if childnode 2, return min(left,right)
         int right=minDepth(root->right);
         
-        if(left>right) return right+1;
-        else return left+1;
+        return min(left,right)+1;
     }
 }
-
