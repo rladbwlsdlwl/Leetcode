1a3fea0c7aec3df4314958fb17ca75161e682833
rladbwlsdlwl <rladbwlsdlwl@naver.com>
2022-03-30 14:52:43 +0900

    python solution(Medium, binarysearch)

--- Searcha2DMatrix.py
+++ Searcha2DMatrix.py
@@ -1,19 +1,23 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        
-        def search_row(arr,key,row):
-            if(row==len(arr)):
-                return False
-            if(arr[row][0]<=key and arr[row][-1]>=key):
-                return row+1 # if row is 0, Recognize False
+        def find_row(i):
+            if(i<len(matrix) and matrix[i][0]<=target):
+                return find_row(i+1)
             else:
-                return search_row(arr,key,row+1)
-        
-        if(len(matrix)==0 or len(matrix[0])==0): return False # blank array
-        
-        
-        check=search_row(matrix,target,0) 
-        if(not check):
+                return i-1
+            
+            
+        def find_col(i):
+            left,right=0,len(matrix[i])-1
+            while left<=right:
+                mid=(left+right)//2
+                if matrix[i][mid]==target:
+                    return True
+                elif matrix[i][mid]<target:
+                    left=mid+1
+                else:
+                    right=mid-1
+                    
             return False
-        
-        return (True if(target in matrix[check-1]) else False) #approach index, sub 1
+            
+        return find_col(find_row(0))
