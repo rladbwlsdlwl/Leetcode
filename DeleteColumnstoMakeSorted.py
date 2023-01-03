7c10620d57043533b87ebfa3023ba19a89e4b57d
rladbwlsdlwl <rladbwlsdlwl@naver.com>
2023-01-03 17:13:38 +0900

    python solution(easy)

---
+++ DeleteColumnstoMakeSorted.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        cnt = 0
+
+        for i in range(len(strs[0])):
+            for j in range(1, len(strs)):
+                if ord(strs[j][i]) - ord(strs[j-1][i]) < 0:
+                    cnt += 1
+                    break
+
+        return cnt 
