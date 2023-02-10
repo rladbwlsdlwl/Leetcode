707a438b296f8722744ee40467d6e49b99c81493
rladbwlsdlwl <rladbwlsdlwl@naver.com>
2023-02-10 18:32:31 +0900

    python solution(Medium, bfs)

---
+++ AsFarfromLandasPossible.py
@@ -0,0 +1,36 @@
+class Solution:
+   def maxDistance(self, grid: List[List[int]]) -> int:
+        # use deque - bfs
+        N = len(grid)
+        dy = [-1, 0, 1, 0]
+        dx = [0, 1, 0, -1]
+
+
+        land = [[0 for _ in range(N)] for _ in range(N)]
+        visited = [[False for _ in range(N)] for _ in range(N)]
+   
+        ans = -1
+        q = deque()
+
+        for i in range(N):
+            for j in range(N):
+                if grid[i][j] == 1:
+                    q.append((i, j, 0))
+
+                    
+        while q:
+            y, x, distance = q.popleft()
+
+            for i in range(4):
+                movey, movex = y+dy[i], x+dx[i]
+                if 0<=movey<N and 0<=movex<N and grid[movey][movex]==0 and not visited[movey][movex]:
+                    visited[movey][movex] = True
+                    q.append((movey, movex, distance+1))
+
+                    ans = max(ans, distance+1)
+
+
+        
+
+        return ans
+                    
