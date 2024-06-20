2eb78ef84a8df961c31554bb1c6ea828cac1388d
rladbwlsdlwl <rladbwlsdlwl@naver.com>
2024-06-20 16:14:34 +0900

    initialze sql file

---
+++ sql/StudentsandExaminations.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select st.student_id, st.student_name, s.subject_name, count(e.subject_name) as attended_exams
+from Students st join Subjects s
+left join Examinations e on st.student_id = e.student_id and s.subject_name = e.subject_name
+group by st.student_id, st.student_name, s.subject_name
+order by st.student_id, s.subject_name
